e8f494383bfb00f7a984225eb7a6176d21dda51c
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-05-20 19:43:44 +0530

    Create binary_serach_through_recursion.c

---
+++ binary_serach_through_recursion.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int bi_ser(int *a,int e,int p,int q)
+{
+	int m;
+	if(p>q) return 0;
+	m=(p+q)/2;
+	if(a[m]==e) return 1;
+	if(a[m]>e)  q=m-1;
+	if(a[m]<e)  p=m+1;
+	m=(p+q)/2;
+	return bi_ser(a,e,p,q);
+}
+int main()
+{
+	int n,i,t,j,e;
+	scanf("%d",&n);
+	int a[n],p=0,q=n-1;
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				t=a[j];
+				a[j]=a[j+1];
+				a[j+1]=t;
+			}
+		}
+	}
+	scanf("%d",&e);
+    if(bi_ser(a,e,p,q))
+    {
+    	printf("found");
+	}
+	else
+	{
+		printf("not found");
+	}
+}
